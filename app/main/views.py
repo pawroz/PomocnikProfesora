f10eb03e5278e2c761c59c5a88616990c82664b2
pawroz <rozplochowskipawel9@gmail.com>
2020-10-18 19:59:48 +0200

    added to new db from interface

--- app/main/views.py
+++ app/main/views.py
@@ -16,8 +16,8 @@ def index():
             db.session.add(user)
             db.session.commit()
             session['known'] = False
-            if current_app.config['FLASKY_ADMIN']:
-                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
+            if current_app.config['PomocnikProfesora_ADMIN']:
+                send_email(current_app.config['PomocnikProfesora_ADMIN'], 'New User',
                            'mail/new_user', user=user)
         else:
             session['known'] = True
@@ -27,4 +27,23 @@ def index():
                            form=form, name=session.get('name'),
                            known=session.get('known', False))
 
-
+@main.route("/student_register",methods=["GET", "POST"])
+def student_register():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+            if current_app.config['POMOCNIKPROFESORA_ADMIN']:
+                send_email(current_app.config['POMOCNIKPROFESORA_ADMIN'], 'New User',
+                           'mail/new_user', user=user)
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('.student_register'))
+    return render_template('student_register.html',
+                           form=form, name=session.get('name'),
+                           known=session.get('known', False))
--- config.py
+++ config.py
@@ -10,9 +10,9 @@ class Config:
         ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    POMOCNIKPROFESORA_MAIL_SUBJECT_PREFIX = '[POMOCNIKPROFESORA]'
+    POMOCNIKPROFESORA_MAIL_SENDER = 'POMOCNIKPROFESORA Admin <POMOCNIKPROFESORA@example.com>'
+    POMOCNIKPROFESORA_ADMIN = os.environ.get('POMOCNIKPROFESORA_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
